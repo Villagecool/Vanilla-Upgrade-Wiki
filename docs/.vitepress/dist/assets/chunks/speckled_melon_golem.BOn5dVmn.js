1d708050642925a91406ec27567b3a91a5026db5
Vlliage <villagercool123@gmail.com>
2025-03-07 18:59:32 -0500

    Tweaks and Mobs page

---
+++ docs/.vitepress/dist/assets/chunks/speckled_melon_golem.BOn5dVmn.js
@@ -0,0 +1,1 @@
+const s="/Vanilla-Upgrade-Wiki/assets/melon_golem._lHAGkgM.png",a="/Vanilla-Upgrade-Wiki/assets/speckled_melon_golem.DPClvDCK.png";export{s as _,a};
---
+++ docs/.vitepress/dist/assets/chunks/termite.D01BpkCU.js
@@ -0,0 +1,1 @@
+const e="/Vanilla-Upgrade-Wiki/assets/termite.DzJgedfc.png";export{e as _};
